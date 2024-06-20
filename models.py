1059b9bbea4a652c9b49a905860887a0570232b2
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-06-20 14:12:14 -0500

    base for api wrote (pre-debug)

--- models.py
+++ models.py
@@ -10,3 +10,14 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "sqlite:///./sbDatabase.db"
 
 Base = declarative_base()
+
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String, index=True)
+    
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    
+    Base.metadata.create_all(bind=engine)
