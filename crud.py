21f3d522c384d6efbd507151f088044820c5785c
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2024-06-20 15:14:29 -0500

    Fit crud.py to sbDatabase

--- crud.py
+++ crud.py
@@ -4,16 +4,52 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
 
-def get_item(db: Session, item_id:int):
-    return db.query(models.Item).filter(model-s.Item.id == item_id).first()
+def get_member(db: Session, member_id:int):
+    return db.query(models.Member).filter(models.Member.memberID == member_id).first()
 
-def get_items(db: Session, skip: int=0, limit: int=10):
-    return db.query(models.Item).offset(skip).limit(limit).all()
+def get_members(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Member).offset(skip).limit(limit).all()
 
-def create_item(db: Session, item:schemas.ItemCreate):
-    db_item = models.Item(name=item.name, description=item.description)
-    db.add(db_item)
+def create_member(db: Session, member:schemas.MemberCreate):
+    db_member = models.Member(comittees=member.comittees, 
+                              name=member.name)
+    db.add(db_member)
     db.commit()
-    db.refresh(db_item)
-    return db_item
+    db.refresh(db_member)
+    return db_member
+
+def get_stock(db: Session, stock_id: int):
+    return db.query(models.Stock).filter(models.Stock.stockID == stock_id).first()
+
+def get_stocks(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Stock).offset(skip).limit(limit).all()
+
+def create_stock(db: Session, stock:schemas.StockCreate):
+    db_stock = models.Stock(tick=stock.tick,
+                            sector=stock.sector,
+                            industry=stock.industry,
+                            companyName=stock.companyName)
+    db.add(db_stock)
+    db.commit()
+    db.refresh(db_stock)
+    return db_stock
+
+def get_trade(db: Session, trade_id:int):
+    return db.query(models.Trade).filter(models.Trade.tradeID == trade_id).first()
+
+def get_trades(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Trade).offset(skip).limit(limit).all()
+
+def create_trade(db: Session, trade:schemas.TradeCreate):
+    db_trade = models.Trade(saleType=trade.saleType, 
+                            dateBought=trade.dateBought,
+                            priceBought=trade.priceBought,
+                            dateDisclosed=trade.dateDisclosed,
+                            delay=trade.delay,
+                            crossover=trade.crossover,
+                            size=trade.size)
+    db.add(db_trade)
+    db.commit()
+    db.refresh(db_trade)
+    return db_trade
 
